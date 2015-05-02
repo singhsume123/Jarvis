feba18fa7f137cf2b84b00900901f8fb52d75e67
jakewier <jakewier@tamu.edu>
2015-05-02 18:06:14 -0500

    updating admin home and edit

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,19 @@
 class AdminsController < ApplicationController
+  before_action :set_advisor_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @admin = Admin.all
+  end
+
   def new
     @admin = Admin.new
+  end
+
+  def show
+  end
+
+  def edit
+  	#render 'admins/edit'
   end
 
   def create
@@ -17,11 +30,31 @@ class AdminsController < ApplicationController
     end
   end
 
-  def detroy
-    @advisor_user.destroy
+  def update
+    respond_to do |format|
+      if @admin.update(admin_params)
+      #if @admin.update_attribute(name: params[:admin][:name])
+        format.html { redirect_to @admin, notice: 'Admin user was successfully updated.' }
+        format.json { render :show, status: :ok, location: @admin }
+      else
+        format.html { render :edit }
+        format.json { render json: @admin.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def admin_params
-      params.require(:admin).permit(:email, :password, :password_confirmation)
+  def detroy
+    @admin.destroy
+  end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_advisor_user
+      @admin = Admin.first
+    end
+    def admin_params
+      params.require(:admin).permit(:email, :password, :password_confirmation, :name, :phone, :fax, :right_sig_url, :mkt_place_url)
+    end
+    def admin_params_update
+      params.require(:admin).permit(:name)
     end
 end
