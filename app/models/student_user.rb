6b27433ec14cc0bc78c1415e754f280e524300c4
tubadude49 <tubadude49@tamu.edu>
2015-05-05 17:11:04 -0500

    CSV

--- app/models/student_user.rb
+++ app/models/student_user.rb
@@ -1,2 +1,10 @@
 class StudentUser < ActiveRecord::Base
+  def self.to_csv(options = {})
+  CSV.generate(options) do |csv|
+    csv << column_names
+    all.each do |product|
+      csv << student_users.attributes.values_at(*column_names)
+    end
+  end
+end
 end
