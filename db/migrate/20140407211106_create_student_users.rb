f1cf0a6116b20f23fc8b9f76ab450d1d2f92cba7
ankurgupta1989 <ankurgupta8907backup@gmail.com>
2015-10-02 11:43:35 -0500

    Adding missing migration of student users table.

---
+++ db/migrate/20140407211106_create_student_users.rb
@@ -0,0 +1,14 @@
+class CreateStudentUsers < ActiveRecord::Migration
+  def change
+    create_table :student_users do |t|
+      t.string   :school_level
+      t.string   :school_name
+      t.string   :team_name
+      t.string   :pay_code
+      t.string   :first_name
+      t.string   :last_name
+      t.string   :team_code
+      t.timestamps null: false
+    end
+  end
+end
