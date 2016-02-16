1709ddc40a5584719cc1ebbaccc23e60f88f1b1e
Sumeet Arora <sumeetsingharora@dhcp-10-202-157-134.tamulink.tamu.edu>
2016-02-15 20:41:08 -0600

    Fixing the admin bug, where anyone with the url can have the access to admin data

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161203113000) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string   "email"
     t.datetime "created_at",      null: false
@@ -76,7 +79,8 @@ ActiveRecord::Schema.define(version: 20161203113000) do
   create_table "teams", force: :cascade do |t|
     t.string  "team_name"
     t.string  "team_code"
-    t.integer "advisor_users_id"
+    t.integer "advisor_user_id"
   end
 
+  add_foreign_key "teams", "advisor_users"
 end
